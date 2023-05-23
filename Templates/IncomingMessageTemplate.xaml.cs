affb66f63455075dbb04fd9548b2b7b99401e1d8
Z0rro228 <lanetsvlad2809@gmail.com>
2023-05-23 22:19:08 +0300

    Added templates

---
+++ Templates/IncomingMessageTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace MessengerApp.Templates;
+
+public partial class IncomingMessageTemplate : ContentView
+{
+	public IncomingMessageTemplate()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Templates/OutgoingMessageTemplate.xaml.cs
@@ -0,0 +1,9 @@
+namespace MessengerApp.Templates;
+
+public partial class OutgoingMessageTemplate : ContentView
+{
+	public OutgoingMessageTemplate()
+	{
+		InitializeComponent();
+	}
+}
