44241fceb391d38a4e504e5f556825011a8fb83e
Z0rro228 <lanetsvlad2809@gmail.com>
2023-05-24 01:37:10 +0300

    Chat messages

--- Templates/TemplateSelector.cs
+++ Templates/TemplateSelector.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MessengerApp.Models;
+using MessengerApp.ViewModels;
 
 namespace MessengerApp.Templates
 {
-    class TemplateSelector
+    public class TemplateSelector : DataTemplateSelector
     {
+        public DataTemplate IncomingMessageTemplate { get; set; }
+        public DataTemplate OutgoingMessageTemplate { get; set; }
+
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            if (item is Message chat)
+            {
+                if (chat.FromUserId != ChatPageViewModel._userId)
+                    return IncomingMessageTemplate;
+                else
+                    return OutgoingMessageTemplate;
+            }
+            throw new NotImplementedException();
+        }
     }
 }
